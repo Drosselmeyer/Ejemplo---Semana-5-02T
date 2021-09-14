c451e0f736b715b164a5af2e1d7870731a2e19e1
unknown <jonathan12458969@gmail.com>
2021-09-14 15:42:40 -0600

    Initial commit

---
+++ Ejemplo 1/Alumno.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_1
+{
+    class Alumno
+    {
+        //Programar las propiedades de forma privada
+        private string Carnet;
+        private string Nombre;
+        private string Direccion;
+        private int NIE;
+
+        //Constructor por defecto
+        public Alumno()
+        {
+            this.Carnet = "";
+            this.Nombre = "";
+            this.Direccion = "";
+            this.NIE = 0;
+        }
+
+        public Alumno(string carnet, string nombre, string direccion, int nie)
+        {
+            this.Carnet = carnet;
+            this.Nombre = nombre;
+            this.Direccion = direccion;
+            this.NIE = nie;
+        }
+
+        //Setter y Getters de las propiedades
+        public void setCarnet(string carnet)
+        {
+            this.Carnet = carnet;
+        }
+
+        public string getCarnet()
+        {
+            return this.Carnet;
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public string getNombre()
+        {
+            return this.Nombre;
+        }
+
+        public void setDireccion(string direccion)
+        {
+            this.Direccion = direccion;
+        }
+
+        public string getDireccion()
+        {
+            return this.Direccion;
+        }
+
+        public void setNIE(int nie)
+        {
+            this.NIE = nie;
+        }
+
+        public int getNIE()
+        {
+            return this.NIE;
+        }
+
+        //Metodos programados
+        public void entregarTareas()
+        { 
+            Console.WriteLine("El alumno "+ this.Nombre + " ha entregado la tarea");
+        }
+
+        public void realizarExamen()
+        {
+            Console.WriteLine("El alumno " + this.Nombre + " ha realizado el examen");
+        }
+
+        public void registrar()
+        {
+            Console.WriteLine("Se registraron los datos");
+        }
+    }
+}
---
+++ Ejemplo 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaramos la primera instancia
+            Alumno alumno1 = new Alumno();
+            Alumno alumno2 = new Alumno("AG123456","Juan", "Soyapango",12345);
+
+
+            Console.WriteLine("Ingrese el nombre del primer alumno");
+            //Leyendo lo que deseo guardar y almacenar
+            alumno1.setNombre(Console.ReadLine());
+            Console.WriteLine("Ingrese la direccion del primer alumno");
+            alumno1.setDireccion(Console.ReadLine());
+            Console.WriteLine("-----------------------------------");
+
+            Console.WriteLine("Ingrese el nombre del segundo alumno");
+            //Leyendo lo que deseo guardar y almacenar
+            alumno2.setNombre(Console.ReadLine());
+            Console.WriteLine("Ingrese la direccion del segundo alumno");
+            alumno2.setDireccion(Console.ReadLine());
+            Console.WriteLine("-----------------------------------");
+
+            //Imprimimos
+            Console.Write("El primer alumno se llama: ");
+            //Estoy mostrando la propiedad Nombre
+            Console.WriteLine(alumno1.getNombre());
+            Console.WriteLine(alumno1.getDireccion());
+
+            Console.Write("El segundo alumno se llama: ");
+            //Estoy mostrando la propiedad Nombre
+            Console.WriteLine(alumno2.getNombre());
+            Console.WriteLine(alumno2.getDireccion());
+
+            Console.WriteLine("-----------------------------------");
+            alumno1.realizarExamen();
+            alumno2.entregarTareas();
+            Console.ReadKey();
+        }
+    }
+}
